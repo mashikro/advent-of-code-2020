3efbd1ca986c9939e6cb6817e4c918fd67c38898
Masha Ikromova <mashikro@gmail.com>
2020-12-01 16:17:32 -0500

    Part 2 of challenge solved

--- expense_report_d1.py
+++ expense_report_d1.py
@@ -1,4 +1,5 @@
-# PART 1
+# --- Part One ---
+
 # find the two entries that sum to 2020 and then multiply those two numbers 
 # together in advent_of_code_expense_report.rtf
 
@@ -21,4 +22,25 @@ def calculate(file):
                 # print((nums[i]*nums[j]))
                 return (nums[i]*nums[j])
 
-calculate("advent_of_code_expense_report.txt")
+# calculate("advent_of_code_expense_report.txt")
+
+# --- Part Two ---
+
+# find three numbers in your expense report that add up to 2020. return their product
+
+
+def calculate2(file):
+    nums = file_to_list(file)
+
+    for i in range(len(nums)):
+        num1 = nums[i]
+        for j in range(i, len(nums)):
+            num2 = nums[j]
+            for k in range(j, len(nums)):
+                num3 = nums[k]
+                print("nums[i]+nums[j]+nums[k]", nums[i]+nums[j]+nums[k])
+                if nums[i]+nums[j]+nums[k] == 2020:
+                    print((nums[i]*nums[j]*nums[k]))
+                    return (nums[i]*nums[j]*nums[k])
+
+calculate2("advent_of_code_expense_report.txt")
